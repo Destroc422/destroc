2e27a5cedcd3f9067808e917dd3d2edd78b05a27
Destroc422 <114114885+Destroc422@users.noreply.github.com>
2022-09-24 13:16:49 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,30 @@
-input.onButtonPressed(Button.AB, function () {
+input.onGesture(Gesture.LogoUp, function () {
+    prueba_1.change(LedSpriteProperty.Y, -1)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    prueba_1.move(-1)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    prueba_1.move(1)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    prueba_1.change(LedSpriteProperty.Y, 1)
+})
+let prueba_1: game.LedSprite = null
+game.setLife(3)
+prueba_1 = game.createSprite(2, 2)
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    game.addScore(1)
+    basic.pause(50000)
+    basic.pause(50000)
+})
+basic.forever(function () {
+    for (let index = 0; index < randint(1, input.lightLevel()); index++) {
+        input.setSoundThreshold(SoundThreshold.Loud, 255)
+    }
     music.setVolume(music.volume())
     for (let index = 0; index < 4; index++) {
         music.playMelody("E D F A B A G F ", 120)
@@ -19,6 +45,51 @@ input.onButtonPressed(Button.AB, function () {
                 music.playMelody("F G A G F E D C ", 120)
                 music.playMelody("D C G A E G C E ", 120)
                 music.playMelody("G A B A G F E D ", 120)
+                music.playMelody("D C G A E G C E ", 120)
+                music.playMelody("G A B A G F E D ", 120)
+                music.playMelody("F G A G F E D C ", 120)
+                music.playMelody("D C G A E G C E ", 120)
+                music.playMelody("G A B A G F E D ", 120)
+                music.playMelody("F G A G F E D C ", 120)
+                music.playMelody("D C G A E G C E ", 120)
+                music.playMelody("E D F A B A G F ", 120)
+                music.playMelody("C D E E D E G F ", 120)
+                for (let index = 0; index < 4; index++) {
+                    for (let index = 0; index < 4; index++) {
+                        music.playMelody("E D F A B A G F ", 120)
+                        music.playMelody("D C G A E G C E ", 120)
+                        music.playMelody("G A B A G F E D ", 120)
+                        music.playMelody("F G A G F E D C ", 120)
+                        music.playMelody("D C G A E G C E ", 120)
+                        music.playMelody("G A B A G F E D ", 120)
+                        music.playMelody("F G A G F E D C ", 120)
+                        music.playMelody("D C G A E G C E ", 120)
+                        music.playMelody("E D F A B A G F ", 120)
+                        music.playMelody("C D E E D E G F ", 120)
+                        for (let index = 0; index < 4; index++) {
+                            music.playMelody("E D F A B A G F ", 120)
+                            music.playMelody("D C G A E G C E ", 120)
+                            music.playMelody("G A B A G F E D ", 120)
+                            music.playMelody("F G A G F E D C ", 120)
+                            music.playMelody("D C G A E G C E ", 120)
+                            music.playMelody("G A B A G F E D ", 120)
+                            music.playMelody("F G A G F E D C ", 120)
+                            music.playMelody("D C G A E G C E ", 120)
+                            music.playMelody("E D F A B A G F ", 120)
+                            music.playMelody("C D E E D E G F ", 120)
+                        }
+                    }
+                    music.playMelody("E D F A B A G F ", 120)
+                    music.playMelody("D C G A E G C E ", 120)
+                    music.playMelody("G A B A G F E D ", 120)
+                    music.playMelody("F G A G F E D C ", 120)
+                    music.playMelody("D C G A E G C E ", 120)
+                    music.playMelody("G A B A G F E D ", 120)
+                    music.playMelody("F G A G F E D C ", 120)
+                    music.playMelody("D C G A E G C E ", 120)
+                    music.playMelody("E D F A B A G F ", 120)
+                    music.playMelody("C D E E D E G F ", 120)
+                }
                 music.playMelody("F G A G F E D C ", 120)
                 music.playMelody("D C G A E G C E ", 120)
                 music.playMelody("E D F A B A G F ", 120)
@@ -34,50 +105,25 @@ input.onButtonPressed(Button.AB, function () {
                     music.playMelody("D C G A E G C E ", 120)
                     music.playMelody("E D F A B A G F ", 120)
                     music.playMelody("C D E E D E G F ", 120)
+                    music.playMelody("F G A G F E D C ", 120)
+                    music.playMelody("D C G A E G C E ", 120)
+                    music.playMelody("G A B A G F E D ", 120)
+                    music.playMelody("F G A G F E D C ", 120)
+                    music.playMelody("D C G A E G C E ", 120)
+                    music.playMelody("E D F A B A G F ", 120)
+                    music.playMelody("C D E E D E G F ", 120)
                 }
             }
             music.playMelody("E D F A B A G F ", 120)
             music.playMelody("D C G A E G C E ", 120)
             music.playMelody("G A B A G F E D ", 120)
-            music.playMelody("F G A G F E D C ", 120)
-            music.playMelody("D C G A E G C E ", 120)
-            music.playMelody("G A B A G F E D ", 120)
-            music.playMelody("F G A G F E D C ", 120)
-            music.playMelody("D C G A E G C E ", 120)
-            music.playMelody("E D F A B A G F ", 120)
-            music.playMelody("C D E E D E G F ", 120)
         }
     }
+    for (let index = 0; index < randint(1, input.lightLevel()); index++) {
+        input.setSoundThreshold(SoundThreshold.Loud, 255)
+    }
+    music.setVolume(music.volume())
+    for (let index = 0; index < 4; index++) {
+        music.playMelody("E D F A B A G F ", 120)
+    }
 })
-for (let index = 0; index < randint(1, input.lightLevel()); index++) {
-    input.setSoundThreshold(SoundThreshold.Loud, 128)
-}
-led.plot(1, 1)
-basic.pause(200)
-led.plot(0, 1)
-basic.pause(200)
-led.plot(0, 0)
-basic.pause(200)
-led.plot(1, 0)
-basic.pause(200)
-led.plot(3, 1)
-basic.pause(200)
-led.plot(4, 1)
-basic.pause(200)
-led.plot(4, 0)
-basic.pause(200)
-led.plot(3, 0)
-basic.pause(200)
-led.plot(0, 3)
-basic.pause(200)
-led.plot(1, 4)
-basic.pause(200)
-led.plot(2, 4)
-basic.pause(200)
-led.plot(3, 4)
-basic.pause(200)
-led.plot(4, 3)
-basic.pause(200)
-led.plot(5, 4)
-basic.pause(200)
-led.plot(6, 4)
